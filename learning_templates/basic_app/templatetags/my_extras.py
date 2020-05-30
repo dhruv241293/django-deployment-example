57882f613363c5997ccff98b33e6b5795f61314a
dhruv241293 <dhruv241293@gmail.com>
2020-05-30 20:18:07 +0530

    'first'

---
+++ learning_templates/basic_app/templatetags/my_extras.py
@@ -0,0 +1,13 @@
+from django import template
+
+register = template.Library()
+
+@register.filter(name='cut')
+
+def cut(value,arg):
+    """
+    this cuts out all value of arg from the string.
+    """
+    return value.replace(arg,'dhruv')
+
+# register.filter('cut',cut)  #second cut is function
